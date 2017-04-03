a59813bf6eb796e3773cbf431960a6b9d0f41fc8
Adam Tache <at201@duke.edu>
2017-04-02 23:11:29 -0400

    Added greeting for user first chat

--- bot/bot.py
+++ bot/bot.py
@@ -1,4 +1,5 @@
 # -*- coding: utf-8 -*-
+from components.facebook_greeter import Greeter
 from components.facebook_replier import Replier
 
 ACCESS_TOKEN = "EAAGAoXpiPKIBAP8f5C4WwSHtigeJ3pYYtEZBvSXO0iqHlGzzRrA5XdhFYfBVgwnAOzAtZAFEtEZBI3ZAErrGobXvZADTvz7ZAYg8SqXD4RPUHHFaflVZAQzXAdvmJr0wQc0HPaVAUH0lsBxV6nsJOoeEgmp7cy9Oza9jHJhD8vQHAZDZD"
@@ -7,6 +8,11 @@ class CampusBot(object):
 
 	def __init__(self):
 		self.replier = Replier(ACCESS_TOKEN)
+		self.greeter = Greeter(ACCESS_TOKEN)
+		self.setup()
+
+	def setup(self):
+		self.greeter.setup()
 
 	def handle(self, sender, message):
 		self.replier.reply_to(sender, message)
---
+++ bot/components/facebook_greeter.py
@@ -0,0 +1,41 @@
+# -*- coding: utf-8 -*-
+import json
+
+from requestor import Requestor
+
+FACEBOOK_GREETER_URL = "https://graph.facebook.com/v2.6/me/thread_settings"
+
+class Greeter(object):
+
+	def __init__(self, access_token):
+		self.access_token = access_token
+		self.requestor = Requestor(FACEBOOK_GREETER_URL)
+
+	def setup(self):
+		params = self._get_params()
+		headers = self._get_headers()
+		payload = self._get_payload()
+		self.requestor.post(params, headers, payload)
+
+	def _get_params(self):
+		return {
+			"access_token": self.access_token
+		}
+
+	def _get_headers(self):
+		return {
+			"Content-Type": "application/json"
+		}
+
+	def _get_payload(self):
+		return json.dumps({
+		  "greeting":[
+			{
+			  "locale":"default",
+			  "text": self._get_greeting_text()
+			}
+		  ] 
+		})
+
+	def _get_greeting_text(self):
+		return "Hello {{user_first_name}}!"
