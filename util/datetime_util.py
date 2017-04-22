c392e3d5cecc4a3c4fbd7d88df21906e5f948665
Adam Tache <at201@duke.edu>
2017-04-22 17:26:28 -0400

    Added some util helper functions for datetime

--- util/datetime_util.py
+++ util/datetime_util.py
@@ -1,23 +1,33 @@
 # -*- coding: utf-8 -*-
 from datetime import datetime
 from dateutil import parser
-from pytz import timezone
+from pytz import timezone, utc
 
 def unicode_to_datetime(datetime_unicode):
 	return parser.parse(datetime_unicode)
 
 def get_datetime_now_usa_east_coast():
-	east_coast = timezone('US/Eastern')
-	return datetime.now(east_coast)
+	return datetime.now(timezone('US/Eastern'))
 
 def get_datetime_localized_to_east_coast(datetime):
-	tzinfo = datetime.tzinfo
-	if tzinfo is None:
-		return datetime
-	return datetime
+	if datetime is None:
+		return None
+	return timezone('US/Eastern').localize(datetime)
 
 def get_readable_datetime_string(datetime):
+	if datetime is None:
+		raise ValueError('Did not provide valid datetime string')
 	return datetime.strftime("%B %d, %Y at %I:%M%p")
 
+def get_readable_date(datetime):
+	if datetime is None:
+		raise ValueError('Did not provide valid datetime string')
+	return datetime.strftime("%B %d")
+
 def datetime_with_current_year(datetime):
+	if datetime is None:
+		raise ValueError('Did not provide valid datetime string')
 	return datetime.replace(year=datetime.today().year)
+
+def are_same_date(datetime1, datetime2):
+	return datetime1.date() == datetime2.date()
